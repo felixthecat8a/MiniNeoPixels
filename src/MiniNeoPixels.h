4afc62da36e20924a654fd3867ba31cd4ec2f6da
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2024-10-20 00:23:39 -0500

    Update MiniNeoPixels.h

--- src/MiniNeoPixels.h
+++ src/MiniNeoPixels.h
@@ -7,16 +7,35 @@
 
 class MiniNeoPixels {
 private:
-  Adafruit_NeoPixel pixels;
-  int _count; // Number of LEDs
+  Adafruit_NeoPixel neopixels;
+  int _count;
 
 public:
-  MiniNeoPixels(int pin,int count);
-  void begin();
-  void begin(int brightness);
-  void adjustBrightness(int brightness);
-  void colorAll(int red, int green, int blue);
-  void colorOne(int num, int red, int green, int blue);
+  Pixels(int pin,int count):neopixels(count, pin, NEO_GRB + NEO_KHZ800) {
+    _count = count; // number of pixels
+  }
+  void begin() {
+    neopixels.begin();
+  }
+  void begin(int brightness) {
+    neopixels.begin();
+    neopixels.setBrightness(brightness);
+  }
+  void adjustBrightness(int brightness) {
+    neopixels.setBrightness(brightness);
+  }
+  void colorAll(int red, int green, int blue) {
+    for (int i = 0; i < _count; i++) {
+      neopixels.setPixelColor(i, red, green, blue);
+    }
+    neopixels.show();
+  }
+  void colorOne(int num, int red, int green, int blue) {
+    if (num >= 0 && num < _count) {
+      neopixels.setPixelColor(num, red, green, blue);
+      neopixels.show();
+    }
+  }
 };
 
 #endif
